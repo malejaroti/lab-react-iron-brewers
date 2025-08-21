d10a95807befcc207565ce7105c8a0a68684814d
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-08-21 20:44:37 +0200

    Bonus: Iteration 4 done. Random beer

--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
-
+import axios from "axios";
 
 function RandomBeersPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
   const [randomBeer, setRandomBeer] = useState(beersJSON[0]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    getRandomBeerData();
+  }, []);
 
   // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
 
+  const getRandomBeerData = async () => {
+    try {
+      setIsFetching(true);
+      const response = await axios.get(`https://ih-beers-api2.herokuapp.com/beers/random`);
+      console.log(response);
+      setRandomBeer(response.data);
+      setIsFetching(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (isFetching) {
+    return <h1>I am bringing the RANDOM beer, wait a sec please</h1>;
+  }
 
-  
   // TASKS:
   // 1. Set up an effect hook to make a request for a random beer from the Beers API.
   // 2. Use axios to make a HTTP request.
   // 3. Use the response data from the Beers API to update the state variable.
-
-
 
   // The logic and the structure for the page showing the random beer. You can leave this as it is.
   return (
@@ -26,12 +42,7 @@ function RandomBeersPage() {
 
       {randomBeer && (
         <>
-          <img
-            src={randomBeer.image_url}
-            alt="beer"
-            height="300px"
-            width="auto"
-          />
+          <img src={randomBeer.image_url} alt="beer" height="300px" width="auto" />
           <h3>{randomBeer.name}</h3>
           <p>{randomBeer.tagline}</p>
           <p>Attenuation level: {randomBeer.attenuation_level}</p>
